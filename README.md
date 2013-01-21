5ebdef17bd3198cd3bbfa0ffefbff63462ca2d60
John Graham-Cumming <jgc@cloudflare.com>
2013-01-21 05:59:29 -0800

    Added wrapper for the OpenSSL RC4 algorithm

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ go-openssl
 
 Go wrapper for some OpenSSL functions in libcrypto
 
-Currently provides a wrapper for MD5 and SHA1 hashes. These are
-considerably faster than the native Go versions.
+Currently provides a wrapper for MD5 and SHA1 hashes and the RC4
+encryption algorithm. These are considerably faster than the native Go
+versions.
 
 My benchmarks have OpenSSL MD5 as 1.5x faster and SHA1 as 5x
-faster on AMD64.
+faster on AMD64. RC4 is 7x faster using OpenSSL.
 
---
+++ rc4/rc4.go
@@ -0,0 +1,44 @@
+package rc4
+
+// #cgo LDFLAGS: -lcrypto
+// #include <openssl/rc4.h>
+import "C"
+
+import (
+	"strconv"
+)
+
+type Cipher struct {
+	key *_Ctype_RC4_KEY
+}
+
+type KeySizeError int
+
+func (k KeySizeError) Error() string {
+	return "rc4: invalid key size " + strconv.Itoa(int(k))
+}
+
+func NewCipher(key []byte) (*Cipher, error) {
+	k := len(key)
+	if k < 1 || k > 256 {
+		return nil, KeySizeError(k)
+	}
+	var c Cipher
+	c.key = &_Ctype_RC4_KEY{}
+	C.RC4_set_key(c.key, C.int(k), (*_Ctype_unsignedchar)(&key[0]))
+
+	return &c, nil
+}
+
+func (c *Cipher) XORKeyStream(dst, src []byte) {
+	C.RC4(c.key, C.size_t(len(dst)), (*_Ctype_unsignedchar)(&src[0]),
+		(*_Ctype_unsignedchar)(&dst[0]))
+}
+
+func (c *Cipher) Reset() {
+	for i := 0; i < 256; i++ {
+		c.key.data[i] = 0
+	}
+	c.key.x = 0
+	c.key.y = 0
+}
